8a5ad92d0661dc8a0dd5c6961ada6c956b9062df
CxllZ <72345758+CxllZ@users.noreply.github.com>
2021-07-27 17:36:24 +0100

    Add files via upload

---
+++ kahoot_flooder.py
@@ -0,0 +1,47 @@
+from kahoot import client
+import threading2, time, requests, os, pyfiglet
+from colorama import Fore, init
+from termcolor import colored
+
+f = open("names.txt", 'r').readlines()
+init()
+banner = colored(pyfiglet.figlet_format("Kahoot Flooder", font="slant"), color="red")
+pin = 0
+ite = 0
+
+os.system("cls" or "clear")
+print(banner, f"{Fore.RED}\n By https://github.com/ycl310/KahootFloodBot")
+print("The kahoot bots will go away once the program is closed!")
+pin = int(input(f"{Fore.GREEN}Enter Kahoot lobby PIN code: "))
+ite = int(input(f"{Fore.GREEN}Enter how many bots u want connected?: "))
+
+if ite > 2000:
+	print(f"{Fore.RED}Cannot exceed 2000 bots, Limit reached!\n{Fore.RED}Automatically setting amount of bots to 2000!\n{Fore.RED}2000 bots might not acutally join!")
+	ite = 2000
+else:
+	pass
+
+
+def thread_func(name: str):
+	def joinHandle():
+		pass
+	bot = client()
+	bot.join(int(pin), str(name))
+	bot.on("joined", joinHandle)
+
+
+def main():
+	t = None
+	for i in range(int(ite)):
+		t = threading2.Thread(target=thread_func, args=(str(f[i]),))
+		t.start()
+		time.sleep(int(0.1))
+	while True:
+		try:
+			pass
+		except KeyboardInterrupt:
+			t.stop()
+			exit()
+
+if __name__ == "__main__":
+	main()
